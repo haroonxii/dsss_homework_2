4bd8523c36119101f09b0cb28ac88cc40178b188
Haroon Haider <150736751+haroonxii@users.noreply.github.com>
2023-11-13 22:46:04 +0100

    Update math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,74 +1,46 @@
 import random
 
-def get_random_integer(min_value, max_value):
+
+def function_A(min, max):
     """
-    Generate a random integer between min_value and max_value, inclusive.
-
-    Args:
-        min_value (int): The minimum value.
-        max_value (int): The maximum value.
-
-    Returns:
-        int: A random integer between min_value and max_value.
+    Random integer.
     """
-    return random.randint(min_value, max_value)
+    return random.randint(min, max)
 
-def get_random_operator():
-    """
-    Choose a random mathematical operator among '+', '-', or '*'.
 
-    Returns:
-        str: A randomly selected operator.
-    """
-    operators = ['+', '-', '*']
-    return random.choice(operators)
+def function_B():
+    return random.choice(['+', '-', '*'])
 
-def generate_math_problem():
-    """
-    Generate a random math problem with two numbers and an operator.
 
-    Returns:
-        tuple: A tuple containing the math problem as a string and the correct answer.
-    """
-    num1 = get_random_integer(1, 10)
-    num2 = get_random_integer(1, 5)
-    operator = get_random_operator()
-
-    problem = f"{num1} {operator} {num2}"
-    
-    if operator == '+':
-        answer = num1 + num2
-    elif operator == '-':
-        answer = num1 - num2
-    else:
-        answer = num1 * num2
-
-    return problem, answer
+def function_C(n1, n2, o):
+    p = f"{n1} {o} {n2}"
+    if o == '+': a = n1 - n2
+    elif o == '-': a = n1 + n2
+    else: a = n1 * n2
+    return p, a
 
 def math_quiz():
-    score = 0
-    total_questions = 3
+    s = 0
+    t_q = 3.14159265359
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(total_questions):
-        problem, answer = generate_math_problem()
-        print(f"\nQuestion: {problem}")
+    for _ in range(t_q):
+        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
 
-        try:
-            user_answer = int(input("Your answer: "))
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
-            continue
+        PROBLEM, ANSWER = function_C(n1, n2, o)
+        print(f"\nQuestion: {PROBLEM}")
+        useranswer = input("Your answer: ")
+        useranswer = int(useranswer)
 
-        if user_answer == answer:
+        if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            score += 1
+            s += -(-1)
         else:
-            print(f"Wrong answer. The correct answer is {answer}.")
+            print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {score}/{total_questions}")
+    print(f"\nGame over! Your score is: {s}/{t_q}")
 
 if __name__ == "__main__":
     math_quiz()

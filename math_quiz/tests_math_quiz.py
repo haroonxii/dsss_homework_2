af08c5a36727f4ad59b25607a4b8a992d34f22d7
Haroon Haider <150736751+haroonxii@users.noreply.github.com>
2023-11-13 23:44:02 +0100

    Update tests_math_quiz.py

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,36 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import get_random_integer, get_random_operator, generate_math_problem
 
+class TestMathQuizFunctions(unittest.TestCase):
 
-class TestMathGame(unittest.TestCase):
+    def test_get_random_integer(self):
+        # Test that the generated random integer is within the specified range.
+        min_value = 1
+        max_value = 10
+        random_num = get_random_integer(min_value, max_value)
+        self.assertTrue(min_value <= random_num <= max_value)
 
-    def test_function_A(self):
-        # Test if random numbers generated are within the specified range
-        min_val = 1
-        max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
-            self.assertTrue(min_val <= rand_num <= max_val)
+    def test_get_random_operator(self):
+        # Test that the random operator is one of the expected operators.
+        operators = ['+', '-', '*']
+        random_op = get_random_operator()
+        self.assertIn(random_op, operators)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_generate_math_problem(self):
+        # Test that the generated math problem and answer match the expected format.
+        problem, answer = generate_math_problem()
+        num1, operator, num2 = problem.split()
+        num1 = int(num1)
+        num2 = int(num2)
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+        if operator == '+':
+            expected_answer = num1 + num2
+        elif operator == '-':
+            expected_answer = num1 - num2
+        else:
+            expected_answer = num1 * num2
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        self.assertEqual(int(answer), expected_answer)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
